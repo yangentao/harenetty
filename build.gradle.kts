a86e8e3e00838c9c81bc0eaa1da561f1ed9f1d6b
Entao <entaoyang@163.com>
2025-05-02 19:02:13 +0800

    1.0.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 group = "io.github.yangentao"
 
-version = "1.0.9"
+version = "1.0.11"
 val artifactName = "harenetty"
 val githubLib = "harenetty"
 val descLib = "Java/Kotlin web, Netty."
@@ -34,7 +34,7 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     api("io.netty:netty-all:4.1.119.Final")
-    api("io.github.yangentao:hare:[1.0.9,)")
+    api("io.github.yangentao:hare:[1.0.11,)")
 }
 
 tasks.test {
@@ -43,13 +43,13 @@ tasks.test {
 kotlin {
     jvmToolchain(21)
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_21
+        jvmTarget = JvmTarget.JVM_11
     }
 }
 java {
     toolchain.languageVersion = JavaLanguageVersion.of(21)
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 mavenPublishing {
